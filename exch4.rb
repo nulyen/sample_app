122ebdb9b5ca44602fe865c660c4ca16ec5739ff
Ron Roth <nulyen@yahoo.com>
2011-08-01 15:55:08 -0400

    ch4 done

---
+++ exch4.rb
@@ -0,0 +1,7 @@
+
+person1 = {:first => "Ron", :last = "Roth"}
+person2 = {:first => "Amanda", :last = "Ferris"}
+person3 = {:first => "Owen", :last = "Roth"}
+params = {:father => person1, :mother => person2, :child => person3}
+
+    
