2ca2437742adf2bf3854fe56a15247f568346b82
Ron Roth <nulyen@yahoo.com>
2011-08-15 16:34:45 -0400

    done ch10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  before_filter :already_signed_in, :only => [:new, :create]
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+    
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -24,5 +33,52 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @title = "Edit user"
+  end
   
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.admin?
+      flash[:error] = "You cannot destroy an admin user."
+    else
+      @user.destroy
+      flash[:success] = "User destroyed."
+    end
+    redirect_to users_path
+  end
+    
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      if !current_user
+        redirect_to(signin_path)
+      else
+        redirect_to(root_path) unless current_user.admin?
+      end
+    end
+
+    def already_signed_in
+        redirect_to(root_path) if current_user
+    end
+                  
 end
